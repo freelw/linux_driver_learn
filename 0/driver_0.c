33f74aed28489b8ee4e266d3d1b529685fc5d577
wangli <wangli@ubuntu.example.com>
2020-04-03 16:38:22 +0800

    update

---
+++ 0/driver_0.c
@@ -0,0 +1,17 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+int __init demo_init(void)
+{
+	printk("---%s---%s---%d---\n", __FILE__, __func__, __LINE__);
+	return 0;
+}
+
+void __exit demo_exit(void)
+{
+	printk("---%s---%s---%d---\n", __FILE__, __func__, __LINE__);
+}
+
+module_init(demo_init);
+module_exit(demo_exit);
+MODULE_LICENSE("GPL");
