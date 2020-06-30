d11d33a32ca41e8d9e7297ae834348eda07cd4b6
freelw <826231693@qq.com>
2020-06-30 20:38:54 +0800

    parameter
    Signed-off-by: freelw <826231693@qq.com>

--- runninglinuxkernel/hello_world/my_test.c
+++ runninglinuxkernel/hello_world/my_test.c
@@ -1,13 +1,27 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+
+static int debug = 1;
+module_param(debug, int , 0644);
+MODULE_PARM_DESC(debug, "enable debugging information");
+
+#define dprintk(args...) \ 
+  if (debug) { \
+    printk(KERN_DEBUG args); \
+  }
+
+static int mytest = 100;
+module_param(mytest, int, 0644);
+MODULE_PARM_DESC(mytest, "test for module parameter");
 static int __init my_test_init(void) {
-  printk("my first kenerl module init\n");
+  dprintk("my first kenerl module init\n");
+  dprintk("module parameter=%d\n", mytest);
   return 0;
 }
 
 static void __exit my_test_exit(void) {
-  printk("goodbye\n");
+  dprintk("goodbye\n");
 }
 
 module_init(my_test_init);
